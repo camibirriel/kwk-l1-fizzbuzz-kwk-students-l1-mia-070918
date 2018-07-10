48359fbd3291fe87c8529c8ad3fae3c852d42c96
camibirriel <camibirriel@gmail.com>
2018-07-10 17:09:01 +0000

    automatically backed up by learn

---
+++ fizzbuzz.rb
@@ -0,0 +1,13 @@
+def fizzbuzz(number) 
+  if (number%3==0)&&(number%5==0)
+    puts"fizzbuzz"
+  elsif (number%3==0)
+    puts "fizz"
+  elsif (number%5==0) 
+    puts "buzz"
+  else
+    puts "nil"
+  end
+end
+fizzbuzz(4) 
+  
